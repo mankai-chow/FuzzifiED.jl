c854fb7c2758cb65cdaa926051d52df8a699407f
Zheng Zhou <goodwill.zhou@gmail.com>
2024-05-27 11:11:39 -0400

    Add support for built-in SparseMatrixCSC formats.

--- src/basics/opmat.jl
+++ src/basics/opmat.jl
@@ -97,3 +97,32 @@ function *(st_fp :: LinearAlgebra.Adjoint{Float64, Vector{Float64}}, mat :: OpMa
     @ccall LibpathFuzzifiED.diag_re_mp_scal_prod_re_(mat.dimd :: Ref{Int64}, mat.dimf :: Ref{Int64}, mat.sym_q :: Ref{Int64}, mat.nel :: Ref{Int64}, mat.colptr :: Ref{Int64}, mat.rowid :: Ref{Int64}, mat.elval :: Ref{Float64}, st_d :: Ref{Float64}, st_fp' :: Ref{Float64}, ovl_ref :: Ref{Float64}) :: Nothing
     return ovl_ref[]
 end
+
+
+"""
+    SparseMatrixCSCFromOpMat(mat :: OpMat{ComplexF64}) :: SparseMatrixCSC{Int64,ComplexF64}
+    SparseMatrixCSCFromOpMat(mat :: OpMat{Float64}) :: SparseMatrixCSC{Int64,Float64}
+
+converts the `OpMat` objects to a `SparseMatrixCSC` object in the `SparseArrays` package.
+"""
+function SparseMatrixCSCFromOpMat(mat :: OpMat)
+    matcsc1 = SparseMatrixCSC(mat.dimf, mat.dimd, mat.colptr, mat.rowid, mat.elval)
+    if (mat.sym_q == 0) 
+        return matcsc1
+    elseif (mat.sym_q == 1)
+        return matcsc1 + adjoint(tril(matcsc1, -1))
+    elseif (mat.sym_q == 2)
+        return matcsc1 + transpose(tril(matcsc1, -1))
+    end
+end
+
+
+"""
+    OpMat(matcsc :: SparseMatrixCSC{Int64,ComplexF64}) :: OpMat{ComplexF64}
+    OpMat(matcsc :: SparseMatrixCSC{Int64,Float64}) :: OpMat{Float64}
+
+converts the `SparseMatrixCSC` object in the `SparseArrays` package to an `OpMat` objects.
+"""
+function OpMat(matcsc :: SparseMatrixCSC)
+    return OpMat{typeof(matcsc.nzval[1])}(matcsc.n, matcsc.m, 0, length(matcsc.rowval), matcsc.colptr, matcsc.rowval, matcsc.nzval)
+end
