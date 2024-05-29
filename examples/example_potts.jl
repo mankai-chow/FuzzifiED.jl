662412e06b4e6bf957b584f1dc44830c64129d47
Zheng Zhou <goodwill.zhou@gmail.com>
2024-05-29 16:20:11 -0400

    Optimise the Potts example.

--- examples/example_potts.jl
+++ examples/example_potts.jl
@@ -1,6 +1,5 @@
 using FuzzifiED
 using LinearAlgebra
-using PrettyTables
 const ω = (-1 + √3im) / 2
 const mat_Z = diagm([1, ω, ω'])
 const mat_h = [ 0 1 1 ; 1 0 1 ; 1 1 0 ]
@@ -20,11 +19,11 @@ for (qn_r, (qn_z2, qn_z3)) in Iterators.product([-1, 1], [(1, 1), (-1, 1), (0, �
     @show qn_r, qn_z2, qn_z3
     bs = GetS3Basis(cfs ; qn_z3, qn_z2, qn_r)
 
-    hmt = Operator(bs, bs, tms_hmt ; red_q = 0, sym_q = 0)
+    hmt = Operator(bs, bs, tms_hmt ; red_q = 1, sym_q = 1)
     @time "Generate sparse matrix" hmt_mat = OpMat(hmt)
     enrg, st = GetEigensystem(hmt_mat, 20)
 
-    l2 = Operator(bs, bs, tms_l2 ; red_q = 0, sym_q = 0)
+    l2 = Operator(bs, bs, tms_l2 ; red_q = 1, sym_q = 1)
     l2_mat = OpMat(l2)
     l2_val = [ st[:, i]' * l2_mat * st[:, i] for i = 1 : length(enrg)]
 
@@ -38,5 +37,7 @@ enrg_0 = result[1][1]
 enrg_T = filter(st -> abs(st[2] - 6) < eps(Float32) && st[3] == "+" && st[4] == 0, result)[1][1]
 result_dim = [ [ 3 * (st[1] - enrg_0) / (enrg_T - enrg_0) ; st] for st in result ]
 result_display = permutedims(hcat(result_dim...))
-
-pretty_table(result_display, header = ["Dim", "Energy", "L2", "Z2", "Z3", "Ry"])
+display(result_display)
+# For better output, change the last line to 
+# using PrettyTables
+# pretty_table(result_display, header = ["Dim", "Energy", "L2", "Z2", "Z3", "Ry"])
