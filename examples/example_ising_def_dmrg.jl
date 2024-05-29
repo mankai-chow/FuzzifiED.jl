5659b3f7a7dc363cbf11930e892da294b984bda5
Zheng Zhou <goodwill.zhou@gmail.com>
2024-05-29 17:16:59 -0400

    Revise the Ising defect example.

--- examples/example_ising_def_dmrg.jl
+++ examples/example_ising_def_dmrg.jl
@@ -28,34 +28,3 @@ os = OpSum()
 os += "Cdag", 1, "C", 2 
 conv = MPO(os, sites)
 @show inner(stpp', conv, stpm)
-exit()
-
-nm = 12
-@show qnu
-exit()
-cfs = GetIsingDefConfs(nm, nm ; def_conf = [1, 1])
-@show cfs.ncf
-bs = GetIsingBasis(cfs ; qn_p = 1, qn_r = 1)
-tms_hmt = GetIsingIntTerms(nm, [4.75, 1.]) - 3.16 * GetXPolTerms(nm)
-hmt = Operator(bs, bs, tms_hmt ; red_q = 1, sym_q = 1)
-hmt_mat = OpMat(hmt ; type = Float64)
-enrg, st = GetEigensystem(hmt_mat, 10)
-@show enrg
-
-cfs = GetIsingDefConfs(nm, nm ; def_conf = [1, -1])
-@show cfs.ncf
-bs = GetIsingBasis(cfs ; qn_p = 1)
-tms_hmt = GetIsingIntTerms(nm, [4.75, 1.]) - 3.16 * GetXPolTerms(nm)
-hmt = Operator(bs, bs, tms_hmt ; red_q = 1, sym_q = 1)
-hmt_mat = OpMat(hmt ; type = Float64)
-enrg, st = GetEigensystem(hmt_mat, 10)
-@show enrg
-
-cfs = GetIsingDefConfs(nm, nm ; def_conf = [1, 0])
-@show cfs.ncf
-bs = GetIsingBasis(cfs ; qn_p = 1)
-tms_hmt = GetIsingIntTerms(nm, [4.75, 1.]) - 3.16 * GetXPolTerms(nm)
-hmt = Operator(bs, bs, tms_hmt ; red_q = 1, sym_q = 1)
-hmt_mat = OpMat(hmt ; type = Float64)
-enrg, st = GetEigensystem(hmt_mat, 10)
-@show enrg
