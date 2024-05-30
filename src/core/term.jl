18ac352793d3ec3e9981d64d59d24cb056066bd4
Zheng Zhou <goodwill.zhou@gmail.com>
2024-05-30 17:02:32 -0400

    Enable the simplification of terms.

--- src/core/term.jl
+++ src/core/term.jl
@@ -77,3 +77,70 @@ Return the Hermitian conjugate of a series of terms. For each term ``Uc^{(p_1)}_
 function adjoint(tms :: Vector{Term})
     return adjoint.(tms)
 end
+
+
+"""
+    function NormalOrder(tm :: Term) :: Vector{Term}
+
+rearrange a term such that 
+- the creation operators must be commuted in front of the annihilation operator 
+- the orbital index of the creation operators are in ascending order and the annihilation operators in descending order. 
+return a list of terms whose result is equal to the original term. 
+"""
+function NormalOrder(tm :: Term)
+    coeff0 = tm.coeff
+    cstr0 = tm.cstr
+    for i = 1 : 2 : length(cstr0) - 3
+        if (cstr0[i] == -1) 
+            cstr1 = deepcopy(cstr0)
+            deleteat!(cstr1, i : i + 1)
+            return(NormalOrder(Term(coeff0, cstr1)))
+        end
+        if (cstr0[i] == 0 && cstr0[i + 2] == 1)
+            if (cstr0[i + 1] == cstr0[i + 3])
+                cstr_nrm = deepcopy(cstr0)
+                cstr_com = deepcopy(cstr0)
+                cstr_nrm[i : i + 1], cstr_nrm[i + 2 : i + 3] = cstr_nrm[i + 2 : i + 3], cstr_nrm[i : i + 1]
+                deleteat!(cstr_com, i : i + 3)
+                return([ NormalOrder(Term(-coeff0, cstr_nrm)) ; 
+                    NormalOrder(Term(coeff0, cstr_com))])
+            else
+                cstr_nrm = deepcopy(cstr0)
+                cstr_nrm[i : i + 1], cstr_nrm[i + 2 : i + 3] = cstr_nrm[i + 2 : i + 3], cstr_nrm[i : i + 1]
+                return(NormalOrder(Term(-coeff0, cstr_nrm)))
+            end
+        elseif (cstr0[i] == cstr0[i + 2])
+            if (cstr0[i + 1] == cstr0[i + 3]) return Term[] end 
+            if ((cstr0[i] == 1) == (cstr0[i + 1] > cstr0[i + 3]))
+                cstr_nrm = deepcopy(cstr0)
+                cstr_nrm[i : i + 1], cstr_nrm[i + 2 : i + 3] = cstr_nrm[i + 2 : i + 3], cstr_nrm[i : i + 1]
+                return(NormalOrder(Term(-coeff0, cstr_nrm)))
+            end
+        end
+    end
+    if length(cstr0) == 0 return Term(coeff0, [-1, -1]) end
+    return Term[tm]
+end
+
+
+"""
+    function SimplifyTerms(tms :: Vector{Term}) :: Vector{Term}
+
+simplifies the sum of terms such that 
+- each term is normal ordered,
+- like terms are combined, and terms with zero coefficients are removed.
+"""
+function SimplifyTerms(tms :: Vector{Term})
+    tms1 = vcat(NormalOrder.(tms)...)
+    sort!(tms1, by = tm -> sum([(tm.cstr[i] + π) * exp(i) for i in eachindex(tm.cstr)]))
+    i = 1
+    while i < length(tms1)
+        if (tms1[i].cstr == tms1[i + 1].cstr)
+            tms1[i].coeff += tms1[i + 1].coeff
+            deleteat!(tms1, i + 1)
+        else
+            i = i + 1
+        end
+    end
+    return filter(tm -> abs(tm.coeff) > 1E-13, tms1)
+end
