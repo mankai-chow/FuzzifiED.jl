9554519da42468ea45df7661edc2af68aac336fc
Zheng Zhou <goodwill.zhou@gmail.com>
2024-11-23 17:23:35 -0500

    Try again to move ITensors to extension.

---
+++ ext/ITensorsExt/ITensorsExt.jl
@@ -0,0 +1,23 @@
+module ITensorsExt 
+
+using FuzzifiED
+using ITensors 
+using ITensorMPS
+
+include("itensors_format.jl")
+export QNDiagFromSites
+export ConfsFromSites
+export TermsFromOpSum
+export OpSumFromTerms
+export SitesFromQNDiag
+export TruncateQNDiag
+include("easy_sweep.jl")
+export SweepOne
+export EasySweep
+export GetMPOSites
+export GetMPO
+include("ar_itensor.jl")
+export TruncateQnu
+export SitesFromQnu
+
+end 
