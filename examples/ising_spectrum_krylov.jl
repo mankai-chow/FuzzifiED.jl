ab34bb69a1ce0e65f6113e084af01b03258a9c35
Zheng Zhou <goodwill.zhou@gmail.com>
2024-11-25 16:09:19 -0500

    Add examples for Cuda and Krylov.

---
+++ examples/ising_spectrum_krylov.jl
@@ -0,0 +1,52 @@
+# This example calculates the spectrum of 3d Ising model on fuzzy sphere at nm = 12
+# by calling the eigsolve function in KrylovKit.jl instead of Arpack.
+# On my table computer, this calculation takes 4.825 s
+
+using FuzzifiED
+const σ1 = [  1  0 ;  0  0 ]
+const σ2 = [  0  0 ;  0  1 ]
+const σx = [  0  1 ;  1  0 ]
+FuzzifiED.ElementType = Float64
+≈(x, y) = abs(x - y) < eps(Float32)
+
+nm = 12
+qnd = [ 
+    GetNeQNDiag(2 * nm), 
+    GetLz2QNDiag(nm, 2) ]
+qnf = [ 
+    GetParityQNOffd(nm, 2, [2, 1], [-1, 1]), 
+    GetFlavPermQNOffd(nm, 2, [2, 1]), 
+    GetRotyQNOffd(nm, 2) ]
+
+tms_hmt = SimplifyTerms(
+    GetDenIntTerms(nm, 2, 2 .* [4.75, 1.], σ1, σ2) - 
+    3.16 * GetPolTerms(nm, 2, σx) )
+tms_l2 = GetL2Terms(nm, 2)
+
+cfs = Confs(2 * nm, [nm, 0], qnd)
+
+result = []
+for P in [1, -1], Z in [1, -1], R in [1, -1]
+    bs = Basis(cfs, [P, Z, R], qnf)
+    hmt = Operator(bs, tms_hmt)
+    hmt_mat = OpMat(hmt)
+    enrg, st = GetEigensystemKrylov(hmt_mat, 20 ; krylovdim = 50, tol = 1E-8)
+
+    l2 = Operator(bs, tms_l2)
+    l2_mat = OpMat(l2)
+    l2_val = [ st[:, i]' * l2_mat * st[:, i] for i in eachindex(enrg)]
+
+    for i in eachindex(enrg)
+        push!(result, round.([enrg[i], l2_val[i], P, Z], digits = 6))
+    end
+end
+
+sort!(result, by = st -> real(st[1]))
+enrg_0 = result[1][1]
+enrg_T = filter(st -> st[2] ≈ 6 && st[3] ≈ 1 && st[4] ≈ 1, result)[1][1]
+result_dim = [ [ 3 * (st[1] - enrg_0) / (enrg_T - enrg_0) ; st] for st in result ]
+for P in (1, -1), Z in (1, -1)
+    display(permutedims(hcat(
+        filter(st -> st[4] ≈ P && st[5] ≈ Z, result_dim)...
+    )))
+end
