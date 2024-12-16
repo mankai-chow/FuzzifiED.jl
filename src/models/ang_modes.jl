53ca1477d1df13631d860bb7ac68faa578d8d913
Zheng Zhou <goodwill.zhou@gmail.com>
2024-12-16 09:55:32 +0800

    Fix typo.

--- src/models/ang_modes.jl
+++ src/models/ang_modes.jl
@@ -1,4 +1,4 @@
-export AngModes, GetElectronMo, GetPairingMo, GetDensityMo, FilterComponen, FilterL2
+export AngModes, GetElectronMod, GetPairingMod, GetDensityMod, FilterComponent, FilterL2
 
 
 """
