d3b2b9d327d3482ab11f37d1a161b38fecf05ebb
Zheng Zhou <goodwill.zhou@gmail.com>
2026-07-22 10:19:39 -0400

    Update the examples.

---
+++ examples/qed_cs52_nf1_spectrum.jl
@@ -0,0 +1,92 @@
+using FuzzifiED
+using FuzzifiED.Fuzzifino
+using FuzzifiEDFullRotation
+FuzzifiED.ElementType = Float64
+
+nmc = 5
+s = (nmc - 1) / 2
+nmf = 3 * nmc - 2
+nmb = 2 * nmc - 1
+FuzzifiED.ObsNormRadSq = nmc 
+
+qnd_c = [
+    SQNDiag(GetNeQNDiag(nmc), 1),
+    SQNDiag(GetLz2QNDiag(nmc, 1), 1), 
+    zero(SQNDiag, nmc, 1),
+    GetBosonNeSQNDiag(nmc, 1)
+]
+qnd_b = [
+    2 * GetBosonNeSQNDiag(1, nmb),
+    GetBosonLz2SQNDiag(1, nmb, 1), 
+    GetBosonNeSQNDiag(1, nmb),
+    SQNDiag(GetNeQNDiag(1), nmb)
+]
+qnd_f = [
+    3 * SQNDiag(GetNeQNDiag(nmf), 1),
+    SQNDiag(GetLz2QNDiag(nmf, 1), 1), 
+    SQNDiag(GetNeQNDiag(nmf), 1),
+    GetBosonNeSQNDiag(nmf, 1)
+]
+
+sec_c = stack([ne1, ((nmc + 1) * ne1) % 2, 0, 0] for ne1 = 0 : nmc)
+sec_b = stack([2ne1, 0, ne1, 0] for ne1 = 0 : nmc)
+sec_f = stack([3ne1, ((nmf + 1) * ne1) % 2, ne1, 0] for ne1 = 0 : nmc) 
+nebm_c = [0 for ne1 = 0 : nmc]
+nebm_b = [ne1 for ne1 = 0 : nmc]
+nebm_f = [0 for ne1 = 0 : nmc]
+
+tms_lzlp_c = STerms.(GetLpLzTerms(nmc, 1))
+tms_lzlp_b = GetBosonLpLzSTerms(nmb, 1)
+tms_lzlp_f = STerms.(GetLpLzTerms(nmf, 1))
+tms_proj_b = GetBosonDenIntSTerms(nmb, 1)
+tms_proj_f = STerms(GetDenIntTerms(nmf, 1, [0, 1]))
+
+sgsp_c = BuildSSegSpace(nmc, 1, nebm_c, sec_c, qnd_c, tms_lzlp_c)
+sgsp_b = BuildSSegSpace(1, nmb, nebm_b, sec_b, qnd_b, tms_lzlp_b, tms_proj_b)
+sgsp_f = BuildSSegSpace(nmf, 1, nebm_f, sec_f, qnd_f, tms_lzlp_f, tms_proj_f)
+
+
+c = GetFermionSObs(nmc, 1, 1)
+f = GetFermionSObs(nmf, 1, 1)
+b = GetBosonSObs(nmb, 1, 1)
+nc = c' * c 
+nf = f' * f 
+nb = b' * b
+
+tms_int_b = SimplifyTerms(GetIntegral(nb * nb))
+tms_int_1 = SimplifyTerms(GetIntegral(nf * Laplacian(nf)))
+tms_pol_f = SimplifyTerms(GetIntegral(nf))
+
+cpd_int_b = SingleSegSCouple(3, 2, tms_int_b, zeros(Int64, 4))
+cpd_int_1 = SingleSegSCouple(3, 3, tms_int_1, zeros(Int64, 4))
+cpd_pol_f = SingleSegSCouple(3, 3, tms_pol_f, zeros(Int64, 4))
+cpd_hop = ContactSCouple([c', b', f], [ 1 2 -3 ; 0 0 0 ; 0 1 -1 ; 0 0 0]) -
+          ContactSCouple([c, b, f'],  [-1 -2 3 ; 0 0 0 ; 0 -1 1 ; 0 0 0])
+cpd_int_e = SingleSegSCouple(3, 1, GetIntegral(nc * nc), zeros(Int64, 4)) +
+            4 * SingleSegSCouple(3, 2, GetIntegral(nb * nb), zeros(Int64, 4)) +
+            9 * SingleSegSCouple(3, 3, GetIntegral(nf * nf), zeros(Int64, 4)) +
+            4 * ContactSCouple([nc, nb, one(SSphereObs)], zeros(Int64, 4, 3)) +
+            6 * ContactSCouple([nc, one(SSphereObs), nf ], zeros(Int64, 4, 3)) +
+            12 * ContactSCouple([one(SSphereObs), nb, nf ], zeros(Int64, 4, 3)); 
+cpd_hmt = PrepareCouple(0.07888 * cpd_int_e + 0.0 * cpd_int_b + 1.0 * cpd_int_1 - 0.1176583770744356 * cpd_hop + 0.39833690759271606 * cpd_pol_f) 
+
+result = []
+for q = -1 : 1, l = 0 : 2
+    sec_tot = [3nmc, 0, nmc + q, 0]
+    cpsp = BuildSCompSpace([sgsp_c, sgsp_b, sgsp_f], sec_tot, 2l)
+    sgop_hmt = BuildSSegOperators([sgsp_c, sgsp_b, sgsp_f], cpd_hmt)
+    cpop_hmt = BuildSCompOperator(cpsp, cpd_hmt, sgop_hmt)
+    nst = 10 - abs(q) - l
+    enrg, st = GetEigensystem(cpop_hmt, nst ; issymmetric = true)
+    for i in eachindex(enrg)
+        push!(result, [enrg[i] / √(2l + 1), l, q])
+    end
+end
+ 
+sort!(result, by = st -> real(st[1])) 
+enrg_0 =  filter(st -> st[3] ≈ 0, result)[1][1]
+enrg_q = (filter(st -> st[3] ≈ -1, result)[1][1] - filter(st -> st[3] ≈ 1, result)[1][1]) / 2
+enrg_1 = (filter(st -> st[2] ≈ 1 && st[3] ≈ 0, result)[1][1] - enrg_0) / 2
+spec = [ [ (st[1] - enrg_0 + st[3] * enrg_q) / enrg_1 ; st] for st in result]
+sort!(spec, by = st -> real(st[1])) 
+display(permutedims(hcat(spec...)))
---
+++ examples/sp3_spectrum.jl
@@ -0,0 +1,63 @@
+using FuzzifiED
+using FuzzifiEDFullRotation
+using LinearAlgebra
+FuzzifiED.ElementType = Float64
+≈(x, y) = abs(x - y) < eps(Float32)
+
+nm = 5
+ne = nm * 2
+nf = 6
+s = (nm - 1) / 2
+qnd_pt = [
+    GetNeQNDiag(nm * 2),
+    GetLz2QNDiag(nm, 2),
+    GetFlavQNDiag(nm, 2, [1, -1])
+]
+tms_lzlp_pt = GetLpLzTerms(nm, 2)
+tms_c2 = GetC2Terms(nm, 2, :SU)
+sec_pt = stack([[ne1, ((nm + 1) * ne1) % 2, ne1 % 2] for ne1 = 0 : ne])
+sec_tot = [ne, 0, 0]
+
+Δ = GetPairingMod(nm, 2, [0 1;0 0])
+n = GetDensityMod(nm, 2, [1 0;0 1])
+tms_intra = GetDenIntTerms(nm, 2, [1.0, 0.26435143448887427, 0.06520453215339449]) - GetPairIntTerms(nm, 2, [0.3798431681470266, 0.0, -0.02191234730375343], [0 1;0 0])
+cpd_nn_inter = 2 * CoupleDecomp([n, n], ConvPsPot(RecouplePsPot(s, [1.0, 0.26435143448887427, 0.06520453215339449]))..., [0 0;0 0;0 0])
+cpd_ΔΔ_inter = CoupleDecomp([Δ', Δ], ConvPsPot(Dict(2s => 0.3798431681470266, 2s-2 => -0.02191234730375343))..., [2 -2;0 0;0 0]) + 
+    CoupleDecomp([Δ, Δ'], ConvPsPot(Dict(2s => 0.3798431681470266, 2s-2 => -0.02191234730375343))..., [-2 2;0 0;0 0])
+cpd_hmt = PrepareCouple(sum([SingleSegCouple(3, p, tms_intra, [0, 0, 0]) for p = 1 : 3]) +
+    sum([InsertSegment(3, [p1, p2], cpd_nn_inter) for p1 = 1 : 3 for p2 = p1 + 1 : 3]) -
+    sum([InsertSegment(3, [p1, p2], cpd_ΔΔ_inter) for p1 = 1 : 3 for p2 = p1 + 1 : 3])) ;
+
+tms_c2_sg = GetC2Terms(nm, 2, :SU) + (nf - 2) / 4 * GetPolTerms(nm, 2)
+cpd_c2_inter = CoupleDecomp([n, n], ConvPsPot(Dict([2s - l => -0.5 for l = 0 : 2s]))..., [0 0;0 0;0 0]) +
+    CoupleDecomp([Δ', Δ], ConvPsPot(Dict([2s - l => -1 for l = 0 : 2 : 2s]))..., [2 -2;0 0;0 0]) + 
+    CoupleDecomp([Δ, Δ'], ConvPsPot(Dict([2s - l => -1 for l = 0 : 2 : 2s]))..., [-2 2;0 0;0 0])
+cpd_c2 = PrepareCouple(sum([SingleSegCouple(3, p, tms_c2_sg, [0, 0, 0]) for p = 1 : 3]) +
+    sum([InsertSegment(3, [p1, p2], cpd_c2_inter) for p1 = 1 : 3 for p2 = p1 + 1 : 3]))
+
+s_rng = collect(0 : 2)
+c2_rng = [Float64[s * (s + 1)] for s in s_rng]
+sgsp = BuildSegSpaces(nm * 2, sec_pt, qnd_pt, tms_lzlp_pt, tms_c2, c2_rng)
+result = []
+for is in [[1,1,1], [1,1,2], [1,1,3], [1,2,2], [1,2,3], [2,2,2]]
+    si = s_rng[is]
+    sgop_hmt = BuildSegOperators(sgsp[is], cpd_hmt)
+    sgop_c2 = BuildSegOperators(sgsp[is], cpd_c2)
+    for l = 0 : 2
+        cpsp = BuildCompSpace(sgsp[is], sec_tot, 2l)
+        cpop_hmt = BuildCompOperator(cpsp, cpd_hmt, sgop_hmt)
+        cpop_c2 = BuildCompOperator(cpsp, cpd_c2, sgop_c2)
+        nst = 10 - sum(si) - l
+        enrg, st = GetEigensystem(cpop_hmt, nst ; issymmetric = true)
+        for i in eachindex(enrg)
+            c2_val = st[:, i]' * (cpop_c2 * st[:, i]) / √(2l + 1)
+            push!(result, [enrg[i] / √(2l + 1), l, c2_val])
+        end
+    end
+end
+
+sort!(result, by = st -> real(st[1]))
+enrg_0 = result[1][1]
+enrg_T = filter(st -> st[2] ≈ 2 && st[3] ≈ 0, result)[1][1]
+spec = unique([ round.([ 3 * (st[1] - enrg_0) / (enrg_T - enrg_0) ; st] .+ √eps(Float64), digits = 6) for st in result ])
+display(permutedims(stack(spec)))
